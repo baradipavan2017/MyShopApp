d224964c509d3df50f62ec2ddcf4e6329aeac23f
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2021-01-07 15:10:28 +0530

    listenign in different screens use of provider in product details

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -9,10 +11,15 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // extracting the id from the namedRoute to productId
-   final productId = ModalRoute.of(context).settings.arguments as String; 
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    //findby logic can be hidden in provider class
+   // changind the listner to flase in buildcontext it dosent not access data everytime
+   // i.e. increase in performance
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
