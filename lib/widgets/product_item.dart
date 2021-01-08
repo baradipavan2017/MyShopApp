afdfa198dd5d4c627e61baf1f5db2f2dd4e8dd99
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2021-01-08 11:15:57 +0530

    using of consumer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,42 +13,44 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //using provider class to send data
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-            print(product.id);
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+    // final product = Provider.of<Product>(context);
+    return Consumer<Product>(
+      builder: (ctx, product,child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.id,
+              );
+              print(product.id);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
+          footer: GridTileBar(
+              backgroundColor: Colors.black87,
+              leading: IconButton(
+                icon: Icon(
+                    product.isFavorite ? Icons.favorite : Icons.favorite_border),
+                color: Theme.of(context).accentColor,
+                onPressed: () {
+                  product.toggleFavoriteStatus();
+                },
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.shopping_cart),
+                color: Theme.of(context).accentColor,
+                onPressed: () {},
+              ),
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              )),
         ),
-        footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
-              icon: Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
-              color: Theme.of(context).accentColor,
-              onPressed: () {
-                product.toggleFavoriteStatus();
-              },
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              color: Theme.of(context).accentColor,
-              onPressed: () {},
-            ),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            )),
       ),
     );
   }
