eaf82a37095bf92308266b46c1330f650419a322
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2021-01-08 11:45:29 +0530

    login for creating favorites (has a flaw)

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
 import '../widgets/products_grid.dart';
 
-
+enum FilterOptions { Favorites, All }
 
 class ProductOverviewScreen extends StatelessWidget {
   // final List<Product> loadedProduct = detailsProducts;
 
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'MyShop',
           style: TextStyle(),
         ),
+        actions: <Widget>[
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorites) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Onlu Favorites'),
+                  value: FilterOptions.Favorites),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: new ProductGrid(),
     );
