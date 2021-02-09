77c5dae18fb005b52aa72269ad55405bddd282db
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2021-02-09 16:13:43 +0530

    creating and adding snackbar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -51,6 +51,19 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).accentColor,
               onPressed: () {
                 cart.addItem(product.id, product.price, product.title);
+                Scaffold.of(context).hideCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Added item to cart"),
+                    duration: Duration(seconds: 3),
+                    action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      },
+                    ),
+                  ),
+                );
               },
             ),
           ),
