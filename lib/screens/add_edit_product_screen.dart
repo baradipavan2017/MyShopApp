299c2838724b33722407a1c92dc2adf6a7c8354c
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2021-02-11 14:21:29 +0530

    saving the data from the user

--- lib/screens/add_edit_product_screen.dart
+++ lib/screens/add_edit_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../providers/product.dart';
 
 class AddEditProductScreen extends StatefulWidget {
   static const routeName = '/add-edit-product-screen';
@@ -12,6 +13,14 @@ class _AddEditProductScreenState extends State<AddEditProductScreen> {
   final _descFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  var _editedProduct = Product(
+    id: null,
+    title: '',
+    description: '',
+    imageUrl: '',
+    price: 0,
+  );
   @override
   void initState() {
     _imageUrlFocusNode.addListener(_updateImageUrl);
@@ -29,21 +38,36 @@ class _AddEditProductScreenState extends State<AddEditProductScreen> {
   }
 
   void _updateImageUrl() {
-    if(!_imageUrlFocusNode.hasFocus){
-      setState(() {
-        
-      });
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
     }
   }
+
+  void _saveForm() {
+    //saves the form in current state
+    _form.currentState.save();
+    print(_editedProduct.title);
+    print(_editedProduct.price);
+    print(_editedProduct.description);
+    print(_editedProduct.imageUrl);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add/Edit Product'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: <Widget>[
               TextFormField(
@@ -53,6 +77,16 @@ class _AddEditProductScreenState extends State<AddEditProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                      //adding title only in this section as there are no other values
+                      id: null,
+                      title: value,
+                      description: _editedProduct.description,
+                      imageUrl: _editedProduct.imageUrl,
+                      price: _editedProduct.price);
+                },
+                
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Price'),
@@ -60,6 +94,15 @@ class _AddEditProductScreenState extends State<AddEditProductScreen> {
                 keyboardType: TextInputType.number,
                 //focus target
                 focusNode: _priceFocusNode,
+                onSaved: (value) {
+                  _editedProduct = Product(
+                      //adding price only in this section as there are no other values
+                      id: null,
+                      title: _editedProduct.title,
+                      description: _editedProduct.description,
+                      imageUrl: _editedProduct.imageUrl,
+                      price: double.parse(value));
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
@@ -68,6 +111,15 @@ class _AddEditProductScreenState extends State<AddEditProductScreen> {
                 keyboardType: TextInputType.multiline,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descFocusNode);
+                },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                      //adding descrpiption only in this section as there are no other values
+                      id: null,
+                      title: _editedProduct.title,
+                      description: value,
+                      imageUrl: _editedProduct.imageUrl,
+                      price: _editedProduct.price);
                 },
               ),
               Row(
@@ -79,6 +131,18 @@ class _AddEditProductScreenState extends State<AddEditProductScreen> {
                       keyboardType: TextInputType.url,
                       textInputAction: TextInputAction.done,
                       focusNode: _imageUrlFocusNode,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                      onSaved: (value) {
+                        _editedProduct = Product(
+                            //adding title only in this section as there are no other values
+                            id: null,
+                            title: _editedProduct.title,
+                            description: _editedProduct.description,
+                            imageUrl: value,
+                            price: _editedProduct.price);
+                      },
                     ),
                   ),
                   Container(
