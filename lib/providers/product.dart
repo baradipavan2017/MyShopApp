33e76d93c092476f4bb2f0228711fbeb66e1a30d
baradipavan <64634366+baradipavan326-dev@users.noreply.github.com>
2021-03-20 18:00:40 +0530

    changing favourite status

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,9 +18,31 @@ class Product with ChangeNotifier {
     @required this.price,
     this.isFavorite = false,
   });
+  void _setFavValue(bool newvalue) {
+    isFavorite = newvalue;
+    notifyListeners();
+  }
 
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    try {
+      final url =
+          'https://myshop-d6854-default-rtdb.firebaseio.com/products/$id.json';
+      final response = await http.patch(
+        url,
+        body: json.encode(
+          {
+            'isFavorite': isFavorite,
+          },
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
